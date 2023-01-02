fba8cdb3de34a1eea70f921864ae813b774b696c
Alfian Vito Anggoro <atokemen12@gmail.com>
2023-01-02 19:57:45 +0700

    update dark mode + fix fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,14 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const backTop = document.querySelector('#backtop')
 
     if(window.pageYOffset > fixedNav){
         header.classList.add('navbar-fixed');
+        backTop.classList.remove('hidden');
     } else {
         header.classList.remove('navbar-fixed');
+        backTop.classList.add('hidden');
     }
 }
 
@@ -29,10 +32,35 @@ buttonDark.addEventListener('click', () => {
     html.classList.add('dark');
     buttonDark.classList.add('hidden');
     buttonLight.classList.remove('hidden');
+    buttonLight.classList.add('flex');
+    localStorage.theme = 'dark';
 })
 
 buttonLight.addEventListener('click', () => {
     html.classList.remove('dark');
     buttonLight.classList.add('hidden');
     buttonDark.classList.remove('hidden');
+    buttonDark.classList.add('flex');
+    localStorage.theme = 'light';
 })
+
+// Klik Di luar hamburger
+window.addEventListener('click', (e) => {
+    if(e.target != hamburger && e.target != navMenu){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+})
+
+// Theme penyimpanan local storage
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    html.classList.add('dark')
+    buttonDark.classList.add('hidden');
+    buttonLight.classList.remove('hidden');
+    buttonLight.classList.add('flex');
+  } else {
+    html.classList.remove('dark')
+    buttonLight.classList.add('hidden');
+    buttonDark.classList.remove('hidden');
+    buttonDark.classList.add('flex');
+  }
